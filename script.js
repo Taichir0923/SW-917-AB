028474c5e71a454a462902e39a1cd8eacb7ceca3
Taichir0923 <crystal19950923@gmail.com>
2021-10-25 11:34:48 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// Өгөгдлийн төрөл
+// 5 Primitive datatypes
+
+// 1. String - текст төвтэй дата - "gfdk", 'gfdgd', "56"
+// 2. Number - тоон төрлийн дата - 1234567890
+// 3. Boolean - true (1), false (0)
+// 4. undefined - тодорхогүй
+// 5. null - хоосон
+
+// variable - хувьсагч
+
+// var ner = "Narada";
+// var nas = 20;
+// var mergejil;
+// // JS reserved words
+// console.log("Сайн байна уу... Миний нэрийг " + ner + " гэдэг. Би " + nas + " настай...");
+// Math operators + - * /
+
+// remainder operator %
+
+// 
+// var x = 5;
+// var y = 6;
+
+// x = x + y; // += 11
+// x += y; // 17
+// x -= y; 
+
+// x++; // x = x + 1;
+// x--; // x = x - 1;
+// 5м өргөнтэй, 8м урттай тэгш өнцөгтийн периметрийг ол...
+// urt2 + urgun2 = diameter2
+// var urgun = 5;
+// var urt = 8;
+// var p = (urgun**2 + urt**2) ** (1/2)
+// var d = Math.sqrt((Math.pow(urgun, 2) + Math.pow(urt, 2)))
+// Math.PI
+// parseInt => Integer
+// parseFloat => float
+
+// 10м радиустай тойргийн урт, талбайг тус тусад нь ол...
+// talbai = PI * r2
+// urt = PI * D
+// var r = +prompt("Радиусын уртыг оруул");
+// var a = Math.PI * Math.pow(r, 2);
+// var l = Math.PI * (r * 2);
+
+// var x = 15;
+// var y = x.toString();
+
+// var z = "32.5"
+// var sum = x + +z
