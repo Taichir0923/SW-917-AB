e596eefc7227d889df365ebc3d37c881b69915d9
taichir0923 <crystal19950923@gmail.com>
2021-11-17 11:01:44 +0800

    update

--- 11.17/script.js
+++ 11.17/script.js
@@ -1,9 +1,16 @@
 // Document 
 const wrap = document.querySelector('.wrap');
+const imgPath = document.querySelector('#img');
+// const zurag = document.createElement('img')
+// zurag.setAttribute('src', 'gfdjk')
 
 
 function createBox(){
     const box = document.createElement('div');
+    const img = document.createElement('img');
+    img.setAttribute('src' , imgPath.value)
     box.setAttribute('class', 'box');
-    wrap.append(box)
+    box.append(img);
+    wrap.append(box);
+    imgPath.value = '';
 }
