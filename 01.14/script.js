9c9187f370eba157bc27b90efd5d5d6f3d3252cb
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2022-01-13 22:25:59 -0500

    update

---
+++ 01.14/script.js
@@ -0,0 +1,20 @@
+class BudgetApp {
+    insertButton;
+    constructor(){
+        this.insertButton = document.querySelector('#insert');
+
+        this.test();
+    }
+
+    insertHandler(){
+        console.log(this.insertButton)
+    }
+
+    test(){
+        this.insertButton.addEventListener('click' , this.insertHandler.bind(this));
+    }
+}
+
+const app = new BudgetApp();
+
+// call , bind , apply
